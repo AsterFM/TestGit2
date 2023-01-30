2abca0d966f5039c4acf0bc5ff87bb5a8a9f1f6b
Verum <asterfm2000@gmail.com>
2023-01-30 10:30:01 +0300

    MulAndDel

--- TestGit2/Program.cs
+++ TestGit2/Program.cs
@@ -3,4 +3,8 @@ a = Convert.ToDouble(Console.ReadLine());
 b = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Resullt: ", a + b);
 Console.WriteLine("Resullt: ", a - b);
+Console.WriteLine("Resullt: ", a * b);
+Console.WriteLine("Resullt: ", a / b);
+
+
 
