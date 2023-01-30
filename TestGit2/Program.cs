1d3509852dfa822150891cb07d2dd88896cbc4b2
Verum <asterfm2000@gmail.com>
2023-01-30 10:28:04 +0300

    PlusAndMinus

--- TestGit2/Program.cs
+++ TestGit2/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double a, b;
+a = Convert.ToDouble(Console.ReadLine());
+b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Resullt: ", a + b);
+Console.WriteLine("Resullt: ", a - b);
+
